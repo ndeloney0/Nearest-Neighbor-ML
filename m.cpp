ff227a3527610dacc1d6e473d36c783c77a7d417
ndeloney0 <55167554+ndeloney0@users.noreply.github.com>
2021-12-09 16:51:13 -0800

    Add documentation

--- m.cpp
+++ m.cpp
@@ -9,12 +9,20 @@
 using namespace std::chrono;
 using namespace std;
 
+/* GenerateFeatures
+        Class containing our data and functions to manipulate our data.
+*/ 
 class GenerateFeatures {
 public:
     GenerateFeatures() {
         accuracy = 0;
     }
+
+/* ReadFile() 
+        Reads the input data.
+*/
     bool ReadFile(string fileName) {
+        data.clear();
         ifstream fin(fileName);
         if (!fin.is_open()) {
             cout << "Error reading from file!" << endl;
@@ -23,18 +31,27 @@ public:
         
         double num;
         vector<double> temp;
+        char endFileChar = (fileName == "seeds_dataset.txt" ? '\n' : '\r');
         while (fin >> num) {
             temp.push_back(num);
-            if (fin.peek() == '\r') {
+            if (fin.peek() == endFileChar) {
                 data.push_back(temp);
                 temp.clear();
             }
         }
+        cout << "Rows: " << data.size() << endl;
 
         fin.close();
+        if (fileName == "seeds_dataset.txt") {
+            ShiftColumns();
+        }
         return true;
     }
 
+/*  NormalizeData()
+        Normalizes the columns of data by subtracting the mean and dividing by the 
+        standard deviation.
+*/
     void NormalizeData() {
         for (int i = 1; i < data[0].size(); i++) {
             double mean = 0, dev = 0;
@@ -54,6 +71,10 @@ public:
         }
     }
 
+/* UpdateBest()
+        Checks the current accuracy against our best saved accuracy
+        and updates the corresponding set if needed.
+*/
     void UpdateBest(const vector<int> &features, float acc) {
         if (acc > accuracy) {
             accuracy = acc;
@@ -61,12 +82,18 @@ public:
         }
     }
 
+/* ShiftColumns()
+        Switches columns 0 and 6 for the seeds dataset.
+*/
     void ShiftColumns() {
         for (int i = 0; i < data.size(); i++) {
             swap(data[i][0], data[i][data[i].size()-1]);
         }
     }
 
+/* OutputCurr()
+        Outputs the current features being checked and corresponding accuracy.
+*/
     void OutputCurr(const vector<int> &currFeatures, float acc) {
         if (!currFeatures.empty()) {
             cout << "[" << currFeatures[0];
@@ -79,6 +106,12 @@ public:
         }
     }
 
+/* LeaveOneOutCrossValidation()
+        Calculates the accuracy of our current features being checked. The distance
+        is calculated as sqrt((featureX1 - featureX2)^2), we check if the class we calculated
+        is correct and if so we add it to the total correct class count and divide it by our
+        total number of instances.
+*/
     float LeaveOneOutCrossValidation(const vector<int> &currFeatures) {
         float pass = 0;
         vector<vector<double> > dataCopy;
@@ -108,10 +141,15 @@ public:
         return pass / data.size();
     }
 
+/* ForwardSelection()
+        Forward selection search: We start from the empty set and add features which
+        give us the best accuracy to the current set. The accuracy is calculated from
+        LeaveOneOutCrossValidation().
+*/
     void ForwardSelection() {
         vector<int> currFeatures, bestFeaturesI;
         float bestAcc, currAcc;
-
+        // Base Case - No features
         currAcc = LeaveOneOutCrossValidation(currFeatures);
         UpdateBest(currFeatures, currAcc);
         OutputCurr(currFeatures, currAcc);
@@ -137,6 +175,11 @@ public:
         OutputCurr(bestFeatures, accuracy);
     }
 
+/* BackwardElimination()
+        We start with all features and for each set, we remove a feature and calculate the
+        accuracy of that set. We move forward with the set which removes the feature that
+        gives the highest accuracy and continue until we reach the empty set.
+*/
     void BackwardElimination() {
         vector<int> currFeatures, bestFeaturesI, allFeatures;
         float currAcc, bestAcc;
@@ -144,7 +187,11 @@ public:
         for (int i = 1; i < data[0].size(); i++) {
             allFeatures.push_back(i);
         }
+        // Base case - all features
         currFeatures = allFeatures;
+        bestAcc = LeaveOneOutCrossValidation(currFeatures);
+        UpdateBest(currFeatures, bestAcc);
+        OutputCurr(currFeatures, bestAcc);
         currFeatures.pop_back();
 
         while (!currFeatures.empty()) {
@@ -172,6 +219,7 @@ public:
             allFeatures = currFeatures;
             currFeatures.pop_back();
         }
+        // No features
         currAcc = LeaveOneOutCrossValidation(currFeatures);
         UpdateBest(currFeatures, currAcc);
         cout << endl << "Best Features: " << endl;
@@ -186,58 +234,33 @@ private:
 
 int main() {
     GenerateFeatures *g = new GenerateFeatures;
-    string fileName = "large81.txt";
-    // cout << "Enter name of file: ";
-    // getline(cin, fileName);
-    // if (!g->ReadFile(fileName)) {
-    //     return 0;
-    // }
-    g->ReadFile("small38.txt");
-    g->NormalizeData();
-    auto start = high_resolution_clock::now();
-    g->ForwardSelection();
-    auto stop = high_resolution_clock::now();
-    double duration = duration_cast<nanoseconds>(stop - start).count();
-    duration *= 1e-9;
-    cout << fixed << "Time taken: " << duration << setprecision(9) << " sec" << endl;
-
-    start = high_resolution_clock::now();
-    g->BackwardElimination();
-    stop = high_resolution_clock::now();
-    duration = duration_cast<nanoseconds>(stop - start).count();
-    duration *= 1e-9;
-    cout << fixed << "Time taken: " << duration << setprecision(9) << " sec" << endl;
-
-    // g->ReadFile("large81.txt");
-    // g->NormalizeData();
-
-    // start = high_resolution_clock::now();
-    // g->ForwardSelection();
-    // duration = duration_cast<nanoseconds>(stop - start).count();
-    // duration *= 1e-9;
-    // cout << fixed << "Time taken: " << duration << setprecision(9) << " sec" << endl;
-
-    // start = high_resolution_clock::now();
-    // g->BackwardElimination();
-    // duration = duration_cast<nanoseconds>(stop - start).count();
-    // duration *= 1e-9;
-    // cout << fixed << "Time taken: " << duration << setprecision(9) << " sec" << endl;
-    // g->ForwardSelection();
-    // while (1) {
-    //     cout << "1) Foward Selection" << endl;
-    //     cout << "2) Backward Elimination" << endl;
-    //     cout << "Else) Exit" << endl;
-    //     int choice;
-    //     cin >> choice;
-    //     if (choice == 1) {
-    //         g->ForwardSelection();
-    //     } else if (choice == 2) {
-    //         g->BackwardElimination();
-    //     } else {
-    //         break;
-    //     }
-    // }
+    string fileName;
+    cout << "Enter name of file: ";
+    getline(cin, fileName);
+    if (!g->ReadFile(fileName)) {
+        return 0;
+    }
+    g->NormalizeData();    
+    
+    while (1) {
+        cout << "1) Foward Selection" << endl;
+        cout << "2) Backward Elimination" << endl;
+        cout << "Else) Exit" << endl;
+        int choice;
+        cin >> choice;
+        auto start = high_resolution_clock::now();
+        if (choice == 1) {
+            g->ForwardSelection();
+        } else if (choice == 2) {
+            g->BackwardElimination();
+        } else {
+            break;
+        }
+        auto stop = high_resolution_clock::now();
+        double duration = duration_cast<nanoseconds>(stop - start).count();
+        duration *= 1e-9;
+        cout << fixed << "Time taken: " << duration << setprecision(9) << " sec" << endl;
+    }
 
     return 0;
 }
-
